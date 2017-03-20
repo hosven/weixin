88b055ca2a38b99b242dac225faf28363d70ff70
wanglu119 <805784956@qq.com>
2017-03-20 17:20:19 +0800

    add wl_flask

--- test/t_wxpy.py
+++ test/t_wxpy.py
@@ -2,16 +2,28 @@
 __author__ = 'wanglu'
 
 from wxpy import get_wechat_logger
+from wxpy import Bot
 
-# 获得 Logger
-logger = get_wechat_logger()
+def t_log():
 
-# 发送警告
-logger.warning('这是一条 WARNING 等级的日志！')
+    # 获得 Logger
+    logger = get_wechat_logger()
 
-# 捕获可能发生的异常，并发送
-try:
-    1 / 0
-except:
-    logger.exception('又出错啦！')
+    # 发送警告
+    logger.warning('这是一条 WARNING 等级的日志！')
 
+    # 捕获可能发生的异常，并发送
+    try:
+        1 / 0
+    except:
+        logger.exception('又出错啦！')
+
+
+def t_get_friend():
+    bot = Bot()
+    fs = bot.friends()
+    for f in fs:
+        print(f)
+
+if __name__ == "__main__":
+   t_get_friend()
